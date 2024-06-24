d2cafda163e7b2df25faf31a8f428549900f8453
Joshua Bamidele <olajoshua775@gmail.com>
2024-06-24 14:51:28 +0100

    We did exit

--- ClassEditor.java
+++ ClassEditor.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
+import java.awt.event.*;
 import java.io.FileReader;
 import java.util.Scanner;
 
@@ -12,10 +10,12 @@ public class ClassEditor implements ActionListener {
     JMenu fileMenu;
     JMenuItem newPage;
     JMenuItem openFile;
+    JMenuItem exit;
     int count;
+    String path; // Assuming this is a class variable that holds the current file path
 
     public ClassEditor() {
-        frame = new JFrame();
+        frame = new JFrame("SimpleFrame");
         frame.setSize(new Dimension(500, 500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocation(new Point(500, 200));
@@ -27,12 +27,11 @@ public class ClassEditor implements ActionListener {
         area.setFont(new Font("serif", Font.BOLD, 14));
         frame.add(new JScrollPane(area));
 
-        JMenuBar menuContainer = new JMenuBar(); // The bar where the menu can be placed
+        JMenuBar menuContainer = new JMenuBar();
         fileMenu = new JMenu("File");
 
         newPage = new JMenuItem("New", 'N');
-        newPage.setAccelerator(
-                KeyStroke.getKeyStroke('N', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        newPage.setAccelerator(KeyStroke.getKeyStroke('N', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
         newPage.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
@@ -42,41 +41,65 @@ public class ClassEditor implements ActionListener {
         });
 
         openFile = new JMenuItem("Open File", 'O');
-        openFile.setAccelerator(
-                KeyStroke.getKeyStroke('O', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        openFile.setAccelerator(KeyStroke.getKeyStroke('O', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
         openFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 JFileChooser fopen = new JFileChooser();
                 int option = fopen.showOpenDialog(frame);
-                int fileToOpen = option;
-                JFileChooser fileOpen = fopen;
-                if (fileToOpen == JFileChooser.APPROVE_OPTION) {
+                if (option == JFileChooser.APPROVE_OPTION) {
                     area.setText("");
                     try {
-                        Scanner scan = new Scanner(new FileReader(fileOpen.getSelectedFile().getPath()));
+                        Scanner scan = new Scanner(new FileReader(fopen.getSelectedFile().getPath()));
                         while (scan.hasNext()) {
                             area.append(scan.nextLine() + "\n");
                         }
-
-                        frame.setTitle(fileOpen.getSelectedFile().getPath() + "- Editor");
-                        // path = fileOpen.getSelectedFile().getPath();
+                        frame.setTitle(fopen.getSelectedFile().getPath() + " - Editor");
+                        path = fopen.getSelectedFile().getPath();
                     } catch (Exception ex) {
                         JOptionPane.showMessageDialog(null, ex.getMessage());
                     }
-                    // Implement open file action here
+                }
+            }
+        });
+
+        exit = new JMenuItem("Exit");
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.ALT_MASK));
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                int option = JOptionPane.showOptionDialog(null, "Do you wish to save?", "Warning",
+                        JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null, null, null);
+                if (option == JOptionPane.YES_OPTION) {
+                    if (path == null) {
+                        saveAsFile();
+                    } else {
+                        saveFile();
+                    }
+                    System.exit(0);
+                } else if (option == JOptionPane.NO_OPTION) {
+                    System.exit(0);
                 }
             }
         });
 
         fileMenu.add(newPage);
         fileMenu.add(openFile);
+        fileMenu.add(exit);
         menuContainer.add(fileMenu);
 
         frame.setJMenuBar(menuContainer);
-
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void saveAsFile() {
+        // Implementation of save as file
+        System.out.println("Save As File logic goes here");
+    }
+
+    private void saveFile() {
+        // Implementation of save file
+        System.out.println("Save File logic goes here");
     }
 
     @Override
@@ -85,6 +108,6 @@ public class ClassEditor implements ActionListener {
     }
 
     public static void main(String[] args) {
-        ClassEditor ce = new ClassEditor();
+        new ClassEditor();
     }
 }
